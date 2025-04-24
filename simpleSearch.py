d6c2b2c225951b8a2f3ed04bb8d4bae055620d85
geadmin <ge-admin@lambda-vector.com>
2025-04-24 13:54:50 -0400

    simple search

---
+++ simpleSearch.py
@@ -0,0 +1,6 @@
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
+
+agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
+
+agent.run("Search for the best music recommendations for a party at the Wayne's mansion.")
+
